625da3e6c1f0bf456e6ea37e8cddc1707887d0ac
wupingping <janet168@126.com>
2022-07-07 22:14:47 +0800

    process

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,15 @@ module.exports = {
   apps: [{
     name: 'meituan',
     script: '/usr/local/bin/npm',
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 1,
     autorestart: true,
-    watch: false
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
   }]
 }
