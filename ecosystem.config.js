f57b470f81a44beceb9a63858f7030185994bb21
wupingping <janet168@126.com>
2022-07-07 22:32:11 +0800

    xg

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,6 @@ module.exports = {
     script: '/usr/local/bin/npm',
     instances: 1,
     autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
+    watch: false
   }]
 }
